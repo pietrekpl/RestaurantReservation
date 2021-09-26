1f09b3b99dfb262f67fa289a89d4056101ef1dc0
Piotr Ludynia <pietrekpl18@gmail.com>
2021-09-26 23:28:27 +0200

    add getmapping in HomeController to move view into reservations.html create reservations.html with simple site view TO-DO (Center image, implement link for reservation)

--- src/main/java/example/projects/restaurant_reservation/HomeController.java
+++ src/main/java/example/projects/restaurant_reservation/HomeController.java
@@ -1,5 +1,7 @@
 package example.projects.restaurant_reservation;
 
+import example.projects.restaurant_reservation.model.User;
+import example.projects.restaurant_reservation.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    private UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/reservations")
+        public String reservations(Model model){
+        User user = userService.get(10000L);
+        model.addAttribute("user", user);
+        return "reservations";
+
     }
 
 }
