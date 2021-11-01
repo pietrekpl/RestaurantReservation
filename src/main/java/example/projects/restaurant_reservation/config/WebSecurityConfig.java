7cf4aad481786c0363de983948814008c00be1e6
Piotr Ludynia <pietrekpl18@gmail.com>
2021-11-01 10:46:58 +0100

    add dependencies for spring security in pom , add security configuration class

---
+++ src/main/java/example/projects/restaurant_reservation/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package example.projects.restaurant_reservation.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+   // private final BCryptPasswordEncoder passwordEncoder;
+
+
+}
